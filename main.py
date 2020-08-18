8074d7c9dc55c033762249aadf071c8750d2b968
Adam Judd <adam.judd@rea-group.com.au>
2020-08-18 22:13:06 +1000

    Remove useless files and plan out workflow
    I've decided not to store things (exclusively) remotely.
    It's far simpler to just clone the entire repo, that way I
    can just treat everything as reading from/writing to local
    storage.

---
+++ main.py
@@ -0,0 +1,13 @@
+# read out the list of necessary sites
+
+# hit the site and store the html
+
+# determine if a snapshot exists
+
+    # if no: build one and write it to file
+
+    # if yes: compare it to the new cached one
+
+        # if same: notify unchanged
+
+        # if different: notify changed
