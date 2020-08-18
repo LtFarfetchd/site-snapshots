d53cc30e10eeb4b287de389a40c2a481ec7dc6c6
Adam Judd <adam.judd@rea-group.com.au>
2020-08-19 00:01:38 +1000

    Fix a pesky auto-generated hash triggering snapshot change notification

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from os import path
 # read out the list of necessary sites
 sites = [site.strip() for site in open('sites.config').readlines()]
 excludedElementTypes = [elementType.strip() for elementType in open('excludedElems.config').readlines()]
+excludedClassNames = [className.strip() for className in open('excludedClasses.config').readlines()]
 
 # hit the site and store the html, converted to snapshot
 cachedContent = {}
 for site in sites:
     response = urlopen(Request(site, headers={'User-Agent': 'Mozilla/5.0'}))
-    cachedContent[site] = buildSnapShotFromHtml(response.read(), excludedElementTypes)
+    cachedContent[site] = buildSnapShotFromHtml(response.read(), excludedElementTypes, excludedClassNames)
 
 for site, cachedSnap in cachedContent.items():
     filename = f'snapshots/{site[site.rindex("//")+2:site.index(".")]}.snap'
--- snapshotBuilder.py
+++ snapshotBuilder.py
@@ -1,9 +1,12 @@
 from bs4 import BeautifulSoup
 
-def buildSnapShotFromHtml(htmlDocument, excludedElems=[]):
+def buildSnapShotFromHtml(htmlDocument, excludedElems=[], excludedClasses=[]):
     soup = BeautifulSoup(htmlDocument, 'html5lib')
     for elementType in excludedElems:
         for element in soup(elementType):
+            element.decompose()
+    for className in excludedClasses:
+        for element in soup.find_all(class_=className):
             element.decompose()
     body = soup.find('body').prettify()
     return body
