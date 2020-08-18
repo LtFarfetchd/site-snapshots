eafede845b755a30d02734b587e9ce8fe919a68c
Adam Judd <adam.judd@rea-group.com.au>
2020-08-18 23:40:03 +1000

    Flesh out the remaining pieces of core function

--- main.py
+++ main.py
@@ -2,25 +2,22 @@ from urllib.request import Request, urlopen
 from snapshotBuilder import buildSnapShotFromHtml
 
 # read out the list of necessary sites
-sites = [site for site in open('sites.config').readlines()]
+sites = [site.strip() for site in open('sites.config').readlines()]
+excludedElementTypes = [elementType.strip() for elementType in open('excludedElems.config').readlines()]
 
 # hit the site and store the html, converted to snapshot
 cachedContent = {}
 for site in sites:
     response = urlopen(Request(site, headers={'User-Agent': 'Mozilla/5.0'}))
-    cachedContent[site] = buildSnapShotFromHtml(response.read())
-    print(cachedContent[site])
+    cachedContent[site] = buildSnapShotFromHtml(response.read(), excludedElementTypes)
 
-# build a snapshot from the html
-
-
-# determine if a snapshot exists
-
-
-    # if no: build one and write it to file
-
-    # if yes: compare it to the new cached one
-
-        # if same: notify unchanged
-
-        # if different: notify changed
+for site, cachedSnap in cachedContent.items():
+    with open(f'snapshots/{site[site.rindex("//")+2:site.index(".")]}.snap', mode='w+') as snapshot:
+        snapshotContents = snapshot.read()
+        if len(snapshotContents) == 0:
+            print(f'New site: {site}')
+            snapshot.write(cachedSnap)
+        else:
+            if (cachedSnap != snapshotContents):
+                print(f'Changed site: {site}')
+                snapshot.write(cachedSnap)
--- snapshotBuilder.py
+++ snapshotBuilder.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup
 
-def buildSnapShotFromHtml(htmlDocument):
+def buildSnapShotFromHtml(htmlDocument, excludedElems=[]):
     soup = BeautifulSoup(htmlDocument, 'html5lib')
+    for elementType in excludedElems:
+        for element in soup(elementType):
+            element.decompose()
     body = soup.find('body').prettify()
     return body
